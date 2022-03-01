098ddf4132110d6ec6125f0dce166006e34ff8a0
raessatech5 <raessatech5@gmail.com>
2022-03-01 19:17:47 +0800

    implement http client timeout and retries w/ exponential backoff

--- Play.Inventory.Service/Program.cs
+++ Play.Inventory.Service/Program.cs
@@ -2,6 +2,8 @@ using Play.Common.MongoDb;
 using Play.Common.Settings;
 using Play.Inventory.Service.Clients;
 using Play.Inventory.Service.Entities;
+using Polly;
+using Polly.Timeout;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,10 +13,23 @@ var serviceSettings = builder.Configuration.GetSection(nameof(ServiceSettings)).
 builder.Services.AddMongo();
 builder.Services.AddMongoRepository<InventoryItem>("inventoryitems");
 
+Random jitterer = new Random();
+
 builder.Services.AddHttpClient<CatalogClient>(client =>
 {
     client.BaseAddress = new Uri("https://localhost:7159");
-});
+})
+    .AddTransientHttpErrorPolicy(builder2 => builder2.Or<TimeoutRejectedException>().WaitAndRetryAsync(
+        5,
+        retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)) + TimeSpan.FromMilliseconds(jitterer.Next(0, 1000)),
+        //onRetry: (outcome, timespan, retryAttempt) =>
+        //{
+        //    // don't do in prod
+        //    var serviceProvider = builder.Services.BuildServiceProvider();
+        //    serviceProvider.GetService<ILogger<CatalogClient>>().LogWarning($"Delaying for {timespan.TotalSeconds} seconds, then making retry {retryAttempt}");
+        //}
+    ))
+    .AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(1));
 
 builder.Services.AddControllers(options =>
 {
